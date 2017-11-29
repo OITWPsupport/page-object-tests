89e8fc917ad6b8e66d66874a42d83a6421d487f7
davidmlentz <davidmlentz@gmail.com>
2017-11-29 14:17:49 -0700

    Initial commit

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+        unset URL
+        unset TITLE
+        unset HEADER
+	unset SITE
+
+export SITE=$1
+
+while IFS='|' read -r url title header
+do
+	export URL=$url
+	export TITLE=$title
+	export HEADER=$header
+	echo "RESULT: $url"
+	result=`npm test`
+	resultstring=`echo $result | cut -d: -f4-99 | cut -d' ' -f2-99`
+	echo $resultstring
+	echo "................."
+	unset URL
+	unset TITLE
+	unset HEADER
+done < data/$SITE
