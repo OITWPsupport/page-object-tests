f1a82c11241a97c053698397e86062eab1aab75d
David M. Lentz <davidmlentz@gmail.com>
2017-12-08 10:39:06 -0700

    Updates the bash script to select 5 random entries to test

--- script_multiple2.sh
+++ script_multiple2.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-#export SITE=$1
-export SITE=oit
+export SITE=$1
 export BROWSER=firefox
 #result=`npm test`
 #echo $result
 
 count=0
+rm -rf results/$1
+mkdir results/$1
+
+sort -R data/$SITE | head -n5 > data/temp
 
 while IFS='|' read -r url title header
 do
@@ -14,6 +17,6 @@ do
         export URL=$url
         export TITLE=$title
         export HEADER=$header
-	(../../20171020/migrationtest/git_repos/migrationtest/node_modules/webdriverio/bin/wdio wdio.conf.js > $count) &
+	(../../20171020/migrationtest/git_repos/migrationtest/node_modules/webdriverio/bin/wdio wdio.conf.js > results/$1/$count) &
 
-done < data/$SITE
+done < data/temp
