3cc777f3ed925d9e94525eeb0de20419beb49c0d
davidmlentz <davidmlentz@gmail.com>
2018-01-16 13:37:53 -0700

    Towards incorporating download, install, and launch of selenium standalone

---
+++ deployscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#curl -O http://selenium-release.storage.googleapis.com/3.5/selenium-server-standalone-3.5.3.jar
+#curl -L https://github.com/mozilla/geckodriver/releases/download/v0.16.0/geckodriver-v0.16.0-macos.tar.gz | tar xz
+nohup java -jar -Dwebdriver.gecko.driver=./geckodriver selenium-server-standalone-3.5.3.jar &
+echo "deployscript.sh has executed"
